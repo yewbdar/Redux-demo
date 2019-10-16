32d15b16fe0649f027c7ddf70b64721d5e19c144
yewbdar <yewbdar.grima@Gmail.com>
2019-10-16 12:11:15 -0500

    store , action and reducer

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('this is the redux ')
+const BUY_CAKE='BUY_CAKE'
+
+
+   
+function buyCake(){
+    return{
+        type:BUY_CAKE,
+        info:'this is the action'
+    }
+}
+//(instioanState , Action) => newState
+
+const instialState={
+    numOfCake:10
+}
+
+const reducer=(state=instialState, action)=>{
+     switch(action.type){
+         case BUY_CAKE :
+             return{
+                 ...state, numOfCake: state.numOfCake - 1
+             }
+             default : return state; 
+     }
+     
+}
